1b10c1d3ab8a1a74e43f175dbee2a2b617a4fde4
aryabov-igs <snap.alex@mail.ru>
2014-10-14 21:59:03 +0400

    registration

---
+++ SportSocial/Scripts/app/blog/shared/services/serializeObj.srvc.js
@@ -0,0 +1,20 @@
+﻿'use strict';
+
+angular.module('app').factory('serializeObj', [function () {
+    var serialize = function (obj, combineSameNamed) {
+        var str = [];
+        for (var p in obj)
+            if (obj.hasOwnProperty(p)) {
+                if (combineSameNamed && Object.prototype.toString.call(obj[p]) === '[object Array]') {
+                    for (var i = 0; i < obj[p].length; i++) {
+                        str.push(encodeURIComponent(p) + "=" + encodeURIComponent(obj[p][i]));
+                    }
+                } else {
+                    str.push(encodeURIComponent(p) + "=" + encodeURIComponent(obj[p]));
+                }
+            }
+        return str.join("&");
+    };
+
+    return serialize;
+}]);
---
+++ SportSocial/Scripts/app/blog/shared/services/token.srvc.js
@@ -0,0 +1,13 @@
+﻿'use strict';
+
+angular.module('app').factory('tokenRqst', [ function () {
+    var tokenName = '__RequestVerificationToken',
+        tokenEl = angular.element('input[type="hidden"][name=' + tokenName + ']'),
+        tokenVal = tokenEl.length ? tokenEl.val() : null,
+        tokenObj = {};
+    tokenObj[tokenName] = tokenVal;
+    return {
+        obj :   tokenObj,
+        val :   tokenVal
+    }
+}]);
