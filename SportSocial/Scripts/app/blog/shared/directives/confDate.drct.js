9ec633da2fbee96f3b0c71e2b54b141f7e429bd4
aryabov-igs <snap.alex@mail.ru>
2014-10-21 21:46:56 +0400

    блог верстка

---
+++ SportSocial/Scripts/app/blog/shared/directives/confDate.drct.js
@@ -0,0 +1,24 @@
+﻿'use strict';
+
+    //------ скролл к текущему элементу
+angular.module('app').directive('confDate', ['$interval', function ($interval) {
+        return {
+            restrict: 'A',
+            template: '<div>minutes - {{minutes}}, seconds - {{seconds}}</div>',
+            replace: true,
+            scope: {
+                confDate: '@'
+            },
+            link: function (scope, element, attrs) {
+                countdownTimer();
+                scope.minutes = scope.confDate;
+                scope.seconds = scope.confDate;
+                function countdownTimer() {
+                    var smsTime = $interval(function () {
+                        scope.minutes = scope.minutes - 1;
+                        scope.seconds = scope.seconds - 2;
+                    }, 1000);
+                }
+            }
+        }
+    }]);
