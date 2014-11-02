ea6f6cea8349c72b35b1c68cd02264a059329a95
aryabov-igs <snap.alex@mail.ru>
2014-11-02 20:32:42 +0400

    user menu

---
+++ SportSocial/Scripts/app/shared/directives/clickOutside.drct.js
@@ -0,0 +1,39 @@
+﻿'use sctict';
+
+// клик не на элементе
+// ---------------
+angular
+    .module('shared')
+    .directive('clickOutside', ['$document', function ($document) {
+        return {
+            restrict: 'A',
+            scope: {
+                clickOutside: '&'
+            },
+            link: function ($scope, elem, attr) {
+                var classList = (attr.outsideIfNot !== undefined) ? attr.outsideIfNot.replace(', ', ',').split(',') : [];
+                if (attr.id !== undefined) classList.push(attr.id);
+                $document.on('click touchstart', function (e) {
+                    var i = 0,
+                        element;
+                    var test1 = e.target;
+                    var test = elem.find(test1);
+                    if (!e.target || elem.find(e.target).length > 0) return;
+
+                    for (element = e.target; element; element = element.parentNode) {
+                        var id = element.id,
+                            classNames = element.className;
+
+                        if (id !== undefined && id !== null) {
+                            for (i = 0; i < classList.length; i++) {
+                                if (id.indexOf(classList[i]) > -1 || classNames.indexOf(classList[i]) > -1) {
+                                    return;
+                                }
+                            }
+                        }
+                    }
+                    $scope.$eval($scope.clickOutside);
+                });
+            }
+        };
+    }]);
--- SportSocial/Scripts/app/shared/directives/datepicker.drct.js
+++ SportSocial/Scripts/app/shared/directives/datepicker.drct.js
@@ -1,12 +1,15 @@
 ﻿'use strict';
 
-    //------ скролл к текущему элементу
-angular.module('shared').directive('datepicker', [function () {
+// скролл к текущему элементу
+// ---------------
+angular
+    .module('shared')
+    .directive('datepicker', [function () {
         return {
             restrict: 'A',
             require: 'ngModel',
             scope: {
-              options: '=datepicker'  
+                options: '=datepicker'
             },
             link: function (scope, element, attrs, ngModel) {
                 element.datetimepicker(scope.options);
