2a73b7440c585d5c4b3cc04745b815cd05b27095
aryabov-igs <snap.alex@mail.ru>
2014-11-04 14:12:27 +0400

    galery, like/dislike

---
+++ SportSocial/Scripts/app/blog/shared/directives/likeDislike.drct.js
@@ -0,0 +1,49 @@
+﻿'use strict';
+
+// лайки/дизлайки
+// ---------------
+angular
+    .module('blog')
+    .directive('likeDislike', ['likeDislikeRqst', 'utilsSrvc', function (likeDislikeRqst, utilsSrvc) {
+        return {
+            restrict: 'A',
+            replace: true,
+            templateUrl: '/Scripts/templates/blog/article/like-dislike.html',
+            scope: {
+                like        :   '@',    // количество лайков
+                dislike     :   '@',    // количество дислайков
+                type        :   '@',    // тип (статья, комментарий)
+                id          :   '@',    // id сущности
+                isLiked     :   '@',    // уже был поставлен лайк
+                isDisliked  :   '@'     // уже был поставлен дислайк
+            },
+            link: function (scope, element, attrs) {
+                var data = {
+                    id: scope.id,
+                    type: scope.type
+                };
+
+                // like/dislike
+                // ---------------
+                scope.rating = function(action) {
+                    data.action = action;
+                    changeRating();
+                }
+
+                // отправка запроса
+                // ---------------
+                function changeRating() {
+                    likeDislikeRqst.send(utilsSrvc.token.add(data))
+                        .then(function(res) {
+                            if (res.data.success) {
+                                if (data.action === 'like') {
+                                    scope.like = res.data.status ? scope.like - 1 : scope.like + 1;
+                                } else if (data.action === 'dislike') {
+                                    scope.dislike = res.data.status ? scope.dislike - 1 : scope.dislike + 1;
+                                }
+                            }
+                        });
+                }
+            }
+        }
+    }]);
---
+++ SportSocial/Scripts/app/blog/shared/services/likeDislike.srvc.js
@@ -0,0 +1,22 @@
+﻿'use strict';
+
+// лайки/дизлайки
+// ---------------
+angular
+    .module('blog')
+    .factory('likeDislikeRqst', ['$http', 'serializeObj', function ($http, serializeObj) {
+        var send = function(action, obj) {
+            return $http({
+                method: 'POST',
+                url: '/Blog/' + action,
+                data: serializeObj(obj),
+                headers: { 'Content-Type': 'application/x-www-form-urlencoded' }
+            });
+        }
+
+        return {
+            send: function(obj) {
+                return send('Rating', obj);
+            }
+        }
+    }]);
