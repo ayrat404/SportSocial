2cbee8b2d64de877de07cd60a2ba67c69418b79f
aryabov-igs <snap.alex@mail.ru>
2014-11-13 00:11:32 +0400

    fixes

---
+++ SportSocial/Scripts/app/blog/modules/rating/rating.ctrl.js
@@ -0,0 +1,33 @@
+﻿'use strict';
+
+// Контроллер для лайков
+// ---------------
+angular.module('blog').controller('RatingCtrl',
+    ['$scope',
+     'ratingRqst',
+     'utilsSrvc',
+function ($scope, ratingRqst, utilsSrvc) {
+
+    // отправка запроса
+    // ---------------
+    $scope.changeRating = function (action) {
+        var data = {
+            id: $scope.id,
+            actionType: action,
+            entityType: $scope.type
+        }
+        ratingRqst.send(utilsSrvc.token.add(data))
+            .then(function (res) {
+                if (res.data.success) {
+                    if (action == 'like') {
+                        $scope.count = $scope.count + 1;
+                        $scope.isLiked = true;
+                    } else {
+                        $scope.count = $scope.count - 1;
+                        $scope.isDisliked = true;
+                    }
+                    $scope.isRated = true;
+                }
+            });
+    }
+}]);
