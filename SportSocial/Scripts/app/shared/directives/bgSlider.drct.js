0ca3ceb2e3662ca164307e506385964b83cca337
aryabov-igs <snap.alex@mail.ru>
2014-11-19 21:46:17 +0400

    minifixes

---
+++ SportSocial/Scripts/app/shared/directives/bgSlider.drct.js
@@ -0,0 +1,12 @@
+﻿'use strict';
+
+// смена картинок на фоне
+// ---------------
+angular.module('shared').directive('bgSlider', ['$timeout', function ($timeout) {
+    return {
+        restrict: 'A',
+        link: function($scope, element, attrs) {
+            
+        }
+    }
+}]);
