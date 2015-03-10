c91d333216c97f7ed7aee5c5da2339dfc6c43624
aryabov-igs <snap.alex@mail.ru>
2015-03-11 00:51:00 +0400

    +

---
+++ SportSocial/Scripts/app/blog/modules/payment/payment.ctrl.js
@@ -0,0 +1,10 @@
+﻿'use strict';
+
+// Контроллер оплаты
+// ---------------
+angular.module('blog').controller('PaymentCtrl',
+    ['$scope',
+     'utilsSrvc',
+function ($scope, utilsSrvc) {
+
+}]);
