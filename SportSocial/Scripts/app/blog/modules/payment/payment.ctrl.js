f3fc71940ae54635b94027fee8b0c85b1bdb2398
aryabov-igs <snap.alex@mail.ru>
2015-03-16 01:15:08 +0400

    pay index send submit

--- SportSocial/Scripts/app/blog/modules/payment/payment.ctrl.js
+++ SportSocial/Scripts/app/blog/modules/payment/payment.ctrl.js
@@ -8,6 +8,8 @@ angular.module('blog').controller('PaymentCtrl',
      'paymentRqst',
 function ($scope, utilsSrvc, paymentRqst) {
 
+    // tarifs description
+    // ---------------
     var desc = {
         1: 'asdasdsd',
         2: '1231231232',
@@ -24,36 +26,11 @@ function ($scope, utilsSrvc, paymentRqst) {
     }
 
     $scope.tarif = 0;
-    $scope.fn = {};
 
     // change description at slider
     // ---------------
     $scope.$watch('tarif', function (val) {
             $scope.sliderText = desc[val];
     });
-
-    // выбрать тариф
-    // ---------------
-    $scope.fn.selectTarif = function () {
-        //$scope.password.er.s404 = false;
-        //$scope.password.er.server = '';
-        paymentRqst.selectTarif(utilsSrvc.token.add({ product: $scope.tarif }))
-            .then(function (res) {
-                if (res.data.success) {
-                    //$scope.password.success = true;
-                    //$scope.password.btnIsDisabled = true;
-                    $timeout(function () {
-                        //$scope.password.success = false;
-                        //$scope.password.btnIsDisabled = false;
-                    }, 3000);
-                } else {
-                    //$scope.password.er.server = res.data.errorMessage;
-                }
-            }, function () {
-                //$scope.password.er.s404 = true;
-            });
-    }
-
-    
 
 }]);
