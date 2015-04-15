33241e45284d8b0afeab4b4f9ab72aa5ac2c165f
Alexey <alex.happyes@gmail.com>
2015-04-15 20:46:46 +0400

    files

---
+++ SportSocial/Scripts/app/shared/directives/aboutVideo.drct.js
@@ -0,0 +1,17 @@
+﻿'use strict';
+
+// автофокус
+// ---------------
+angular.module('shared').directive('aboutVideo', ['$timeout', function ($timeout) {
+    return {
+        restrict: 'A',
+        link: function (scope, element, attrs) {
+            angular.element(element).on('click', function() {
+                bootbox.dialog({
+                    title: 'Заголовок',
+                    message: '<iframe width="460" height="315" src="//www.youtube.com/embed/zWc41BbjlZ4" frameborder="0" allowfullscreen class="payment__video"></iframe>'
+                });
+            });
+        }
+    }
+}]);
