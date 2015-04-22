dd84928a5fb1b1150878522ccf1b8ba250fe37cc
Alexey <alex.happyes@gmail.com>
2015-04-22 21:08:59 +0400

    small fixes

--- SportSocial/Scripts/app/shared/directives/aboutVideo.drct.js
+++ SportSocial/Scripts/app/shared/directives/aboutVideo.drct.js
@@ -8,8 +8,8 @@ angular.module('shared').directive('aboutVideo', ['$timeout', function ($timeout
         link: function (scope, element, attrs) {
             angular.element(element).on('click', function() {
                 bootbox.dialog({
-                    title: 'Заголовок',
-                    message: '<iframe width="460" height="315" src="//www.youtube.com/embed/zWc41BbjlZ4" frameborder="0" allowfullscreen class="payment__video"></iframe>'
+                    title: 'О сайте',
+                    message: '<iframe width="100%" height="315" src="//www.youtube.com/embed/zWc41BbjlZ4" frameborder="0" allowfullscreen class="payment__video"></iframe>'
                 });
             });
         }
