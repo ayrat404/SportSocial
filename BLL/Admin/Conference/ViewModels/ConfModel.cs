7bf6b3bdd28ea3fdf894358514a9b359d29efde5
ayrat <ayrat404@gmail.com>
2015-04-26 19:47:46 +0300

    *

---
+++ BLL/Admin/Conference/ViewModels/ConfModel.cs
@@ -0,0 +1,16 @@
+using System.ComponentModel.DataAnnotations;
+using DAL.DomainModel.EnumProperties;
+
+namespace BLL.Admin.Conference.ViewModels
+{
+    public class ConfModel: CreateConfModel
+    {
+        [Required]
+        public int Id { get; set; }
+
+        [Required]
+        public ConfStatus Status { get; set; }
+
+        public int Stamp { get; set; }
+    }
+}
