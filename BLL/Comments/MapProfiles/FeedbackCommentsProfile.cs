0ffb5e9871b4dfeef73ca38b69dd0c06bb89376b
ayrat <ayrat404@gmail.com>
2015-05-14 23:09:25 +0300

    fix 2 feedback comment

--- BLL/Comments/MapProfiles/FeedbackCommentsProfile.cs
+++ BLL/Comments/MapProfiles/FeedbackCommentsProfile.cs
@@ -1,25 +1,25 @@
-﻿using AutoMapper;
+using AutoMapper;
 using BLL.Comments.Objects;
-using DAL.DomainModel.BlogEntities;
+using DAL.DomainModel.FeedBackEntities;
 
 namespace BLL.Comments.MapProfiles
 {
-    public class BlogCommentsProfile: Profile
+    public class FeedbackCommentsProfile: Profile
     {
         protected override void Configure()
         {
-            CreateMap<BlogComment, Comment>()
+            CreateMap<FeedbackComment, Comment>()
                 .ForMember(dest => dest.Id, opt => opt.MapFrom(src => src.Id))
                 .ForMember(dest => dest.Avatar, opt => opt.MapFrom(src => src.User.Profile.Avatar))
                 .ForMember(dest => dest.Date, opt => opt.MapFrom(src => src.Created.ToString()))
                 .ForMember(dest => dest.Name, opt => opt.MapFrom(src => src.User.Name))
                 .ForMember(dest => dest.CommentFor,
-                        opt => opt.MapFrom(src => src.CommentForId.HasValue ? new CommentFor()
-                            {
-                                Id = src.CommentForId.Value,
-                                Name = src.CommentFor.User.Name,
-                            }
-                            : null))
+                    opt => opt.MapFrom(src => src.CommentForId.HasValue ? new CommentFor()
+                    {
+                        Id = src.CommentForId.Value,
+                        Name = src.CommentFor.User.Name,
+                    }
+                        : null))
                 .ForMember(dest => dest.Text, opt => opt.MapFrom(src => src.Text));
         }
     }
