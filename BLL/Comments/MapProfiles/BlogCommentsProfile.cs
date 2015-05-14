4f876e79bd2c31d49280228a5f2a1a363792aeae
ayrat <ayrat404@gmail.com>
2015-05-14 23:08:52 +0300

    fix feedback comment

--- BLL/Comments/MapProfiles/BlogCommentsProfile.cs
+++ BLL/Comments/MapProfiles/BlogCommentsProfile.cs
@@ -1,6 +1,7 @@
 ﻿using AutoMapper;
 using BLL.Comments.Objects;
 using DAL.DomainModel.BlogEntities;
+using DAL.DomainModel.FeedBackEntities;
 
 namespace BLL.Comments.MapProfiles
 {
@@ -9,6 +10,25 @@ namespace BLL.Comments.MapProfiles
         protected override void Configure()
         {
             CreateMap<BlogComment, Comment>()
+                .ForMember(dest => dest.Id, opt => opt.MapFrom(src => src.Id))
+                .ForMember(dest => dest.Avatar, opt => opt.MapFrom(src => src.User.Profile.Avatar))
+                .ForMember(dest => dest.Date, opt => opt.MapFrom(src => src.Created.ToString()))
+                .ForMember(dest => dest.Name, opt => opt.MapFrom(src => src.User.Name))
+                .ForMember(dest => dest.CommentFor,
+                        opt => opt.MapFrom(src => src.CommentForId.HasValue ? new CommentFor()
+                            {
+                                Id = src.CommentForId.Value,
+                                Name = src.CommentFor.User.Name,
+                            }
+                            : null))
+                .ForMember(dest => dest.Text, opt => opt.MapFrom(src => src.Text));
+        }
+    }
+    public class FeedbackCommentsProfile: Profile
+    {
+        protected override void Configure()
+        {
+            CreateMap<FeedbackComment, Comment>()
                 .ForMember(dest => dest.Id, opt => opt.MapFrom(src => src.Id))
                 .ForMember(dest => dest.Avatar, opt => opt.MapFrom(src => src.User.Profile.Avatar))
                 .ForMember(dest => dest.Date, opt => opt.MapFrom(src => src.Created.ToString()))
