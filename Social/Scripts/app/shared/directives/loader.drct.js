8c593753962c23330bb4533ddadd5e779409aa76
AlexeyRyabov <alex.happyes@gmail.com>
2015-08-13 13:44:03 +0300

    landing start

--- Social/Scripts/app/shared/directives/loader.drct.js
+++ Social/Scripts/app/shared/directives/loader.drct.js
@@ -1,32 +1,33 @@
 ﻿'use strict';
 
 angular.module('shared')
-    .directive('loader', [function () {
-    var spinnerClass = 'loader timer-loader',
-        containerClass = 'loader-container',
-        wrapClass = 'loader-wrap';
+    .directive('loader', [
+        function() {
+            var spinnerClass = 'loader timer-loader',
+                containerClass = 'loader-container',
+                wrapClass = 'loader-wrap';
 
-        return {
-        restrict: 'A',
-        scope: {
-            loader: '=loader'
-        },
-        link: function(scope, element, attrs) {
-            scope.$watch('loader', function(newVal, oldVal) {
-                if (newVal != oldVal) {
-                    var $el = angular.element(element);
-                    if (newVal === true) {
-                        $el.addClass(containerClass);
-                        $el.append(
-                            angular.element('<div>', { class: wrapClass }).append(
-                            angular.element('<div>', { class: spinnerClass, text: 'Загрузка...' })));
-                    } else if (newVal === false) {
-                        $el.removeClass(containerClass);
-                        $el.find('.' + wrapClass).remove();
-                    }
-
+            return {
+                restrict: 'A',
+                scope: {
+                    loader: '=loader'
+                },
+                link: function(scope, element, attrs) {
+                    scope.$watch('loader', function(newVal, oldVal) {
+                        if (newVal != oldVal) {
+                            var $el = angular.element(element);
+                            if (newVal === true) {
+                                $el.addClass(containerClass);
+                                $el.append(
+                                    angular.element('<div>', { class: wrapClass }).append(
+                                        angular.element('<div>', { class: spinnerClass, text: 'Загрузка...' })));
+                            } else if (newVal === false) {
+                                $el.removeClass(containerClass);
+                                $el.find('.' + wrapClass).remove();
+                            }
+                        }
+                    });
                 }
-            });
+            }
         }
-    }
-}]);
+    ]);
--- Social/Scripts/app/socialApp/app.js
+++ Social/Scripts/app/socialApp/app.js
@@ -4,4 +4,10 @@ angular.module('socialApp.controllers', []);
 angular.module('socialApp.services', []);
 angular.module('socialApp.directives', []);
 
-angular.module('socialApp', ['socialApp.controllers', 'socialApp.services', 'socialApp.directives']);
+angular.module('socialApp', [
+    'socialApp.controllers',
+    'socialApp.services',
+    'socialApp.directives'])
+    .run(function() {
+        angular.element('body').css('opacity', 1);
+    });
