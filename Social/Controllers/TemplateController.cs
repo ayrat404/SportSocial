1f232179b68f25d8376adbd801e29911cf575c2e
ayrat <ayrat@skytender.ru>
2015-08-28 16:52:17 +0300

    +

---
+++ Social/Controllers/TemplateController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Social.Controllers
+{
+    public class TemplateController : Controller
+    {
+        // GET: Template
+        public ActionResult Index(string path)
+        {
+            return View("~/views/template/"+ path + ".cshtml");
+        }
+    }
+}
