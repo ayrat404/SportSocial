2c0f96c37a226b7319af28b638e370d395c05d05
ayrat <ayrat@skytender.ru>
2015-09-23 14:48:40 +0300

    B; фиксы

--- BLL/Comments/MapProfiles/CommentsProfile.cs
+++ BLL/Comments/MapProfiles/CommentsProfile.cs
@@ -28,7 +28,7 @@ namespace BLL.Comments.MapProfiles
                 .ForMember(dest => dest.Avatar, opt => opt.MapFrom(src => src.ByFortress ? LoginService.DefaultFortressAvatar : src.User.Profile.Avatar))
                 .ForMember(dest => dest.Date, opt => opt.MapFrom(src => src.Created.ToString()))
                 .ForMember(dest => dest.Created, opt => opt.MapFrom(src => src.Created))
-                .ForMember(dest => dest.UserInfo, opt => opt.MapFrom(src => src.User))
+                .ForMember(dest => dest.Author, opt => opt.MapFrom(src => src.User))
                 .ForMember(dest => dest.Name, opt => opt.MapFrom(src => src.ByFortress ? "Fortress" : src.User.Name))
                 .ForMember(dest => dest.Text, opt => opt.MapFrom(src => src.Text));
 
